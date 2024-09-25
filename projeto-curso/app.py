05386570ca857e2b5e899cad744119ab95718917
m4nuzinh4 <163332616+m4nuzinh4@users.noreply.github.com>
2024-09-24 21:04:36 -0400

    Update app.py

--- projeto-curso/app.py
+++ projeto-curso/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Lista para armazenar os cursos cadastrados
 cursos = []
 
 @app.route('/')
@@ -11,7 +10,6 @@ def index():
 
 @app.route('/cadastro', methods=['POST'])
 def cadastro():
-    # Captura os dados do formulário
     nome = request.form['nome']
     plataforma = request.form['plataforma']
     descricao = request.form['descricao']
@@ -20,7 +18,6 @@ def cadastro():
     nivel = request.form['nivel']
     categoria = request.form['categoria']
     
-    # Cria um dicionário para o curso
     curso = {
         'nome': nome,
         'plataforma': plataforma,
@@ -31,16 +28,14 @@ def cadastro():
         'categoria': categoria
     }
     
-    # Adiciona o curso à lista
     cursos.append(curso)
     
-    # Redireciona para a página inicial
     return redirect(url_for('index'))
 
 @app.route('/excluir/<int:index>', methods=['POST'])
 def excluir(index):
     if 0 <= index < len(cursos):
-        del cursos[index]  # Remove o curso da lista
+        del cursos[index] 
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
